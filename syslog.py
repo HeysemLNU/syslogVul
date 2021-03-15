2af611de545a6fe2b6e2e6cb92a99ac4ee0c28fa
unknown <ab224hv@student.lnu.se>
2021-03-15 22:27:21 +0100

    Now can write to file

--- syslog.py
+++ syslog.py
@@ -7,12 +7,24 @@ sysFilter = "touch"
 
 sysSOCKET = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 sysSOCKET.bind((sysIP,sysPORT))
-
+print("Syslog Server has started")
 while 1==1:
     recData, recAdd = sysSOCKET.recvfrom(sysBUFFER)
-    print("message: ", recData)
-    stringRecieved = recData
-    if sysFilter in stringRecieved
-        print("Recieved")
+    stringRecieved = recData.decode('utf-8')
+    if sysFilter in stringRecieved:
+        sourceIP = recAdd[0]
+        splitCommand = stringRecieved.split(" ", 1)
+        the_file = open(splitCommand[1],"w")
+        sysSOCKET.close()
+        secondSocket = ocket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        secondSocket.bind((sysIP,sysPORT))
+        while 1==1:
+            recDataTwo, recAddTwo = secondSocket.recvfrom(sysBUFFER)
+            secondSourceIP = recAddTwo[0]
+            if sourceIP == secondSourceIP:
+                 the_file.write(recDataTwo.decode('utf-8'))
+            else:
+                break
+        secondSocket.close()
 
 sysSOCKET.close()
