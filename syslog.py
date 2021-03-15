2f35875f3ca8887d60f377471b8cf394ee482202
unknown <ab224hv@student.lnu.se>
2021-03-15 18:06:34 +0100

    another try

--- syslog.py
+++ syslog.py
@@ -3,12 +3,16 @@ import socket
 sysIP = "192.168.1.1"
 sysPORT = 514
 sysBUFFER = 1024
+sysFilter = "touch"
 
 sysSOCKET = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 sysSOCKET.bind((sysIP,sysPORT))
 
 while 1==1:
     recData, recAdd = sysSOCKET.recvfrom(sysBUFFER)
-    print("message: " % recData)
+    print("message: ", recData)
+    stringRecieved = recData
+    if sysFilter in stringRecieved
+        print("Recieved")
 
 sysSOCKET.close()
